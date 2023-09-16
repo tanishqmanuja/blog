72214cab11b7350ecdb0a2eac590369c66facd9e
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-16 12:34:09 +0530

    refactor: remove unused logo image

--- src/config.ts
+++ src/config.ts
@@ -12,13 +12,6 @@ export const SITE: Site = {
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
 
-export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 216,
-  height: 46,
-};
-
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
