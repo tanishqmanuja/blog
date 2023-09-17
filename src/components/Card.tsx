5af38facdcc4bc501dc14ebe0f98a34086f066d3
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-17 12:56:19 +0530

    feat: better card view transition name logic

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import type { CollectionEntry } from "astro:content";
 import Datetime from "./Datetime";
+import { slugifyStr } from "@/utils/slugify";
 
 export interface Props {
   href?: string;
@@ -11,7 +12,13 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
   const { title, date, description } = frontmatter;
   return (
     <li className="list-card">
-      <a href={href}>{secHeading ? <h2>{title}</h2> : <h3>{title}</h3>}</a>
+      <a href={href}>
+        {secHeading ? (
+          <h2 style={{ viewTransitionName: slugifyStr(title) }}>{title}</h2>
+        ) : (
+          <h3 style={{ viewTransitionName: slugifyStr(title) }}>{title}</h3>
+        )}
+      </a>
       <Datetime datetime={date} />
       <p>{description}</p>
     </li>
