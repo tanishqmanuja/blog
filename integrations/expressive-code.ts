81a03b5c11d7e550f35b431d71cc76175f87a55c
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-26 12:34:02 +0530

    style: format integrations

--- integrations/expressive-code.ts
+++ integrations/expressive-code.ts
@@ -2,14 +2,13 @@ import type { AstroIntegration } from "astro";
 import astroExpressiveCode from "astro-expressive-code";
 
 export const tqblogExpressiveCode = (): AstroIntegration =>
-	astroExpressiveCode({
-		theme: "one-dark-pro",
-    styleOverrides: {
-      
-    },
+  astroExpressiveCode({
+    theme: "one-dark-pro",
+    styleOverrides: {},
     frames: {
+      showCopyToClipboardButton: true,
       styleOverrides: {
         frameBoxShadowCssValue: "none",
-      }
+      },
     },
-	});
+  });
